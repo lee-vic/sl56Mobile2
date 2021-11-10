e492b4e64d80be518028e980c644e4d590fd4cb6
josion1020 <35375094@qq.com>
2021-11-10 17:56:33 +0800

    报价查询结果列表区分出口价和进口价

--- src/app/pages/member/calculation-list/calculation-list.page.ts
+++ src/app/pages/member/calculation-list/calculation-list.page.ts
@@ -8,10 +8,28 @@ import { Router } from '@angular/router';
 })
 export class CalculationListPage implements OnInit {
   calculateResultList:any;
+  tabsData:any;
+  currentTabIndex=0;
   constructor(  private router: Router) { }
 
   ngOnInit() {
     this.calculateResultList=JSON.parse(localStorage.getItem("CalculationResult"));
+    let tempData=new Array();
+    this.calculateResultList.forEach(element => {
+      var tData=null;
+      if(tempData.length>0){
+        tData=tempData.find(p=>p.PriceType==element.PriceType);
+      }
+      if(tData==null){
+        tData = new Object();
+        tData.PriceList = new Array();
+        tData.PriceType=element.PriceType;
+        tempData.push(tData);
+      }
+      tData.PriceList.push(element);
+    });
+    this.tabsData=tempData;
+    console.log(this.tabsData);
   }
  
   detail(item){
