126634407ec8cbf01c500f6a146913e98656d302
josion1020 <35375094@qq.com>
2021-11-11 14:38:32 +0800

    报价计算结果页面，调整出口价选项卡为第一个选项卡

--- src/app/pages/member/calculation-list/calculation-list.page.ts
+++ src/app/pages/member/calculation-list/calculation-list.page.ts
@@ -15,8 +15,10 @@ export class CalculationListPage implements OnInit {
   ngOnInit() {
     this.calculateResultList=JSON.parse(localStorage.getItem("CalculationResult"));
     let tempData=new Array();
+    let emptyObj = new Object();
+    tempData.push(emptyObj);
     this.calculateResultList.forEach(element => {
-      var tData=null;
+      let tData=null;
       if(tempData.length>0){
         tData=tempData.find(p=>p.PriceType==element.PriceType);
       }
@@ -24,10 +26,19 @@ export class CalculationListPage implements OnInit {
         tData = new Object();
         tData.PriceList = new Array();
         tData.PriceType=element.PriceType;
-        tempData.push(tData);
+        //若存在出口价，则把出口价显示在第一个选项卡
+        if(element.PriceType=="出口价")
+        {
+          tempData[0]=tData;
+        }else{
+          tempData.push(tData);
+        }
       }
       tData.PriceList.push(element);
     });
+    if(tempData[0]==emptyObj){
+      tempData.splice(0,1);
+    }
     this.tabsData=tempData;
     console.log(this.tabsData);
   }
