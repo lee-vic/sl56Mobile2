aeea34a9a9e4136496fc2036a0294d9155ea9835
josion1020 <353750904@qq.com>
2023-02-16 17:25:39 +0800

    货物轨迹按包裹号显示

--- src/app/pages/member/delivery-record-detail/delivery-record-detail.page.ts
+++ src/app/pages/member/delivery-record-detail/delivery-record-detail.page.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { DeliveryRecordDetailService } from 'src/app/providers/delivery-record-detail.service';
-import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
-import { NavController, ActionSheetController, AlertController, ToastController } from '@ionic/angular';
-import { ProblemService } from 'src/app/providers/problem.service';
+import { Component, OnInit } from "@angular/core";
+import { DeliveryRecordDetailService } from "src/app/providers/delivery-record-detail.service";
+import { ActivatedRoute, NavigationExtras, Router } from "@angular/router";
+import {
+  NavController,
+  ActionSheetController,
+  AlertController,
+  ToastController,
+} from "@ionic/angular";
+import { ProblemService } from "src/app/providers/problem.service";
 @Component({
-  selector: 'app-delivery-record-detail',
-  templateUrl: './delivery-record-detail.page.html',
-  styleUrls: ['./delivery-record-detail.page.scss'],
+  selector: "app-delivery-record-detail",
+  templateUrl: "./delivery-record-detail.page.html",
+  styleUrls: ["./delivery-record-detail.page.scss"],
 })
 export class DeliveryRecordDetailPage implements OnInit {
   data: any;
@@ -14,16 +19,18 @@ export class DeliveryRecordDetailPage implements OnInit {
   tab = "1";
   isReturn = true;
   canGoBack: boolean = true;
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     private router: Router,
     public service: DeliveryRecordDetailService,
     public problemService: ProblemService,
     public alertCtrl: AlertController,
     public toastCtrl: ToastController,
     public actionSheetController: ActionSheetController,
-    private route: ActivatedRoute) {
+    private route: ActivatedRoute
+  ) {
     //公众号
-    this.route.queryParams.subscribe(p => {
+    this.route.queryParams.subscribe((p) => {
       if (p && p.push) {
         if (p.push == "true") {
           this.canGoBack = false;
@@ -33,92 +40,127 @@ export class DeliveryRecordDetailPage implements OnInit {
   }
 
   ngOnInit() {
-    this.id = +this.route.snapshot.paramMap.get('id');
+    this.id = +this.route.snapshot.paramMap.get("id");
 
     //不能返回的视为站外链接
-    this.service.getDetail(!this.canGoBack, this.id).subscribe(res => {
-      this.data = res;
-      this.isReturn = this.data.IsReturnCustomer;
-      console.log(this.data);
-    }, (err) => {
-
-      //  let toast = this.toastCtrl.create({
-      //    message: err.message,
-      //    position: 'middle',
-      //    duration: 2000
-      //  });
-      //  toast.present();
-    });
+    this.service.getDetail(!this.canGoBack, this.id).subscribe(
+      (res) => {
+        this.data = res;
+        if (this.data.IsShowPackageTracks) {
+          this.data.PackageTracks = JSON.parse(
+            this.data.PackageTracksJsonString
+          );
+          this.data.PackageTracks.forEach((element) => {
+            element.open = false;
+          });
+          console.log("PackageTracks:", this.data.PackageTracks);
+        }
+        this.isReturn = this.data.IsReturnCustomer;
+        console.log(this.data);
+      },
+      (err) => {
+        //  let toast = this.toastCtrl.create({
+        //    message: err.message,
+        //    position: 'middle',
+        //    duration: 2000
+        //  });
+        //  toast.present();
+      }
+    );
   }
   applyReturn() {
-    this.navCtrl.navigateForward("/member/return-apply", { queryParams: { type: 0,ids:this.data.ObjectId } })
+    this.navCtrl.navigateForward("/member/return-apply", {
+      queryParams: { type: 0, ids: this.data.ObjectId },
+    });
   }
   chat() {
     let extras: NavigationExtras = {
       state: {
         receiveGoodsDetailId: this.data.ObjectId,
         messages: this.data.ChatRecords,
-      }
-    }
-    this.router.navigate(["/member/chat/1"], extras)
-    
+      },
+    };
+    this.router.navigate(["/member/chat/1"], extras);
   }
-  more(){
+  more() {
     this.presentActionSheet();
   }
   async presentActionSheet() {
     const actionSheet = await this.actionSheetController.create({
-      header: '请选择',
-      buttons: [{
-        text: '我要退货',
-        handler: () => {
-          this.navCtrl.navigateForward("/member/return-apply", { queryParams: { type: 0 ,ids:this.data.ObjectId} })
-        }
-      }, {
-        text: '我要暂扣',
-        handler: () => {
-          this.alertCtrl.create({
-            header: '确定要暂扣吗',
-            message: '货物暂扣后，将暂停所有操作流程，由于此操作造成的时效问题，一律由客户自身承担',
-            buttons: [
-              {
-                text: '取消',
-                handler: () => {
-                  console.log('Disagree clicked');
-                }
-              },
-              {
-                text: '确定',
-                handler: () => {
-                  this.problemService.addProblem(this.id).subscribe(res => {
-                    if (res.Success == false) {
-                      this.toastCtrl.create({
-                        message: res.Message,
-                        position: 'middle',
-                        duration: 2000
-                      }).then(p => p.present());
-                    }
-                    else {
-                      this.toastCtrl.create({
-                        message: "已成功扣件",
-                        position: 'middle',
-                        duration: 2000
-                      }).then(p => p.present());
-                    }
-                  });
-                }
-              }
-            ]
-          }).then(p => p.present());
-        }
-      }, {
-        text: '取消',
-        role: 'cancel',
-        handler: () => {
-          console.log('Cancel clicked');
-        }
-      }]
+      header: "请选择",
+      buttons: [
+        {
+          text: "我要退货",
+          handler: () => {
+            this.navCtrl.navigateForward("/member/return-apply", {
+              queryParams: { type: 0, ids: this.data.ObjectId },
+            });
+          },
+        },
+        {
+          text: "我要暂扣",
+          handler: () => {
+            this.alertCtrl
+              .create({
+                header: "确定要暂扣吗",
+                message:
+                  "货物暂扣后，将暂停所有操作流程，由于此操作造成的时效问题，一律由客户自身承担",
+                buttons: [
+                  {
+                    text: "取消",
+                    handler: () => {
+                      console.log("Disagree clicked");
+                    },
+                  },
+                  {
+                    text: "确定",
+                    handler: () => {
+                      this.problemService
+                        .addProblem(this.id)
+                        .subscribe((res) => {
+                          if (res.Success == false) {
+                            this.toastCtrl
+                              .create({
+                                message: res.Message,
+                                position: "middle",
+                                duration: 2000,
+                              })
+                              .then((p) => p.present());
+                          } else {
+                            this.toastCtrl
+                              .create({
+                                message: "已成功扣件",
+                                position: "middle",
+                                duration: 2000,
+                              })
+                              .then((p) => p.present());
+                          }
+                        });
+                    },
+                  },
+                ],
+              })
+              .then((p) => p.present());
+          },
+        },
+        {
+          text: "取消",
+          role: "cancel",
+          handler: () => {
+            console.log("Cancel clicked");
+          },
+        },
+      ],
     });
     await actionSheet.present();
+  }
+  trackToggle(packageTrackItem) {
+    this.data.PackageTracks.forEach((element) => {
+      if (element.PackageId == packageTrackItem.PackageId) {
+        element.open = !element.open;
+      } else {
+        element.open = false;
+      }
+    });
   }
 }
